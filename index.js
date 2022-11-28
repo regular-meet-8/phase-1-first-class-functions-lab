e3d05ac953b3be03b3f797eaa7aae0367406fa24
regular-meet-8 <eddiekim444@gmail.com>
2022-11-28 13:26:42 +0300

    functionslab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(){
+
+    return drivers.slice(0,2)
+}
+const returnLastTwoDrivers = function(){
+return drivers.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(x){
+    const quadruple = (function (x){
+        return x * 4
+    })
+    return quadruple();
+
+}
+function createFareMultiplier(y){
+    const fareDoubler =( function (y){
+        return y * 2
+    
+    })
+    return fareDoubler()
+    
+    }
+    function createFareMultiplier(z){
+        const fareTripler = (function (z){
+            return y * 3
+        
+        })
+        return fareTripler()
+        
+        }
+        function selectDifferentDrivers(drivers, returnLastTwoDrivers ){
+            return selectDifferentDrivers();
+            if(selectDifferentDrivers[1]= returnFirstTwoDrivers){
+                return selectDifferentDrivers();
+
+            }
+        }
